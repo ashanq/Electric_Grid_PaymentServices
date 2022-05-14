0874e96773dc5f88721b5e57ce9257eda652ac48
ashanq <79820774+ashanq@users.noreply.github.com>
2022-05-15 00:21:07 +0530

    Adding the java files

--- src/main/java/model/Payment.java
+++ src/main/java/model/Payment.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.sql.Connection;
+
+import com.PaymentAPI;
+import com.mysql.cj.Session;
+
+import java.sql.*;
+import java.sql.Statement;
+import java.util.Base64;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Payment {
 
 }
