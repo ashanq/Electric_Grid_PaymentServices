1fd93ed5eaa3ecdb927c9cc509044a04685b8c32
ashanq <79820774+ashanq@users.noreply.github.com>
2022-05-15 00:22:22 +0530

    Connect the DB

--- src/main/java/model/Payment.java
+++ src/main/java/model/Payment.java
@@ -13,5 +13,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Payment {
+	
+	//A common method to connect to the DB
+			private Connection connect()
+			{
+				Connection con = null;
+				try
+				{
+					Class.forName("com.mysql.jdbc.Driver");
+
+					//Provide the correct details: DBServer/DBName, username, password
+					con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+				}
+				catch (Exception e)
+				{e.printStackTrace();}
+				return con;
+			}
+
+
 
 }
